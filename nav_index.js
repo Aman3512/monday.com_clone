deb4dd46110e6953ab5874d8b4277f3aebdae791
sahill-1 <sv260397@gmail.com>
2022-07-24 00:22:44 +0530

    day-5

---
+++ nav_index.js
@@ -0,0 +1,54 @@
+import { sahil_navbar, products,usecases,features,resources } from "./navbar.js";
+document.getElementById("sahil_navbar").innerHTML = sahil_navbar();
+
+/////////////
+document.getElementById("leftimg").addEventListener("click", () => {
+window.location.href = "index.html"
+})
+
+document.getElementById("navbar-display-board").innerHTML = null;
+let p = 0;
+let u=0;
+let f=0;
+let r=0;
+
+document.getElementById("products").addEventListener("click", () => {
+  if (p == 0) {
+    document.getElementById("navbar-display-board").innerHTML = products();
+    p++;
+  } else {
+    document.getElementById("navbar-display-board").innerHTML = null;
+    p = 0;
+  }
+});
+
+document.getElementById("usecase").addEventListener("click", () => {
+  if (u == 0) {
+    document.getElementById("navbar-display-board").innerHTML = usecases();
+    u++;
+  } else {
+    document.getElementById("navbar-display-board").innerHTML = null;
+    u = 0;
+  }
+});
+
+document.getElementById("features").addEventListener("click", () => {
+  if (f == 0) {
+    document.getElementById("navbar-display-board").innerHTML = features();
+    f++;
+  } else {
+    document.getElementById("navbar-display-board").innerHTML = null;
+    f = 0;
+  }
+});
+
+document.getElementById("resources").addEventListener("click", () => {
+  if (r == 0) {
+    document.getElementById("navbar-display-board").innerHTML = resources();
+    r++;
+  } else {
+    document.getElementById("navbar-display-board").innerHTML = null;
+    r = 0;
+  }
+});
+
